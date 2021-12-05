a5bac77c32a3a158000c9ed2303aa2bac33a03d3
Pablo Suarez <brekopablo@gmail.com>
2021-12-04 18:51:46 -0600

    Inicializacion del Repositario

---
+++ analisis_multivariante.R
@@ -0,0 +1,16 @@
+main <- function() {
+  # Importacion de datos
+  
+  # Limpieza de datos
+  
+  # Intervalos de confianza
+  
+  # Analisis de componentes principales
+  
+  # Analisis de factores
+  
+  # Analisis Cluster
+  
+}
+
+main()
