126790aa6644642c63285c85f7bd93e9c1df5435
Pablo Suarez <brekopablo@gmail.com>
2021-12-04 19:12:12 -0600

    Importacion, Limpieza y Visualizacion

--- analisis_multivariante.R
+++ analisis_multivariante.R
@@ -1,8 +1,13 @@
 main <- function() {
   # Importacion de datos
-  
+  datos <- obt_datos()
+  # Tabulación de los datos
+  View(datos)
   # Limpieza de datos
-  
+  x <- datos[, 4:16]
+  Y <- datos[, 0:3]
+  View(x)
+  View(Y)
   # Intervalos de confianza
   
   # Analisis de componentes principales
@@ -11,6 +16,15 @@ main <- function() {
   
   # Analisis Cluster
   
+}
+
+obt_datos <- function() {
+  # Incializamos y declaramos la ruta del proyecto
+  ruta <- "C:/Users/breko/Documents/Practicas/Programacion_Ciencia_Datos/proyecto3"
+  setwd(dir = ruta)
+  # Importamos los datos
+  datos <- read.csv(file = "cereal.csv", header = T)
+  return(datos)
 }
 
 main()
